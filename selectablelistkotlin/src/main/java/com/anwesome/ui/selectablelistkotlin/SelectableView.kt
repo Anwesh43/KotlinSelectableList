d6379cc2ab318e9311c49b2f36383db1fdec6e86
Anwesh43 <anweshthecool0@gmail.com>
2017-08-02 23:03:34 +0530

    adding fix for calling listener in uithread

--- selectablelistkotlin/src/main/java/com/anwesome/ui/selectablelistkotlin/SelectableView.kt
+++ selectablelistkotlin/src/main/java/com/anwesome/ui/selectablelistkotlin/SelectableView.kt
@@ -1,5 +1,6 @@
 package com.anwesome.ui.selectablelistkotlin
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
@@ -11,7 +12,7 @@ import android.view.View
 /**
  * Created by anweshmishra on 02/08/17.
  */
-class SelectableView(ctx:Context,var text:String,var handler:AnimHandler,vararg var listeners: OnSelectedListener):View(ctx) {
+class SelectableView(var ctx:Context,var text:String,var handler:AnimHandler,vararg var listeners: OnSelectedListener):View(ctx) {
     var time = 0
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
     var selectableItem:SelectableItem?=null
@@ -24,19 +25,27 @@ class SelectableView(ctx:Context,var text:String,var handler:AnimHandler,vararg 
     }
     fun update() {
         selectableItem?.update()
-        if(selectableItem?.stoppedUpdating()?:false && listeners.size == 1) {
-            when(selectableItem?.scale) {
-                0.0f -> {
-                    listeners[0].onUnSelect(selectableItem?.text?:"")
-                }
-                1.0f -> {
-                    listeners[0].onSelect(selectableItem?.text?:"")
+        postInvalidate()
+    }
+    fun stopped():Boolean {
+        var condition = selectableItem?.stoppedUpdating()?:true
+        if(condition) {
+            if(selectableItem?.stoppedUpdating()?:false && listeners.size == 1) {
+                var activity = context as Activity
+                activity.runOnUiThread {
+                    when (selectableItem?.scale) {
+                        0.0f -> {
+                            listeners[0].onUnSelect(selectableItem?.text ?: "")
+                        }
+                        1.0f -> {
+                            listeners[0].onSelect(selectableItem?.text ?: "")
+                        }
+                    }
                 }
             }
         }
-        postInvalidate()
+        return condition
     }
-    fun stopped():Boolean = selectableItem?.stoppedUpdating()?:true
 
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
