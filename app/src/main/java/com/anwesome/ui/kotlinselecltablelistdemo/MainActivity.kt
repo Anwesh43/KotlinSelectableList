4961511eb88e7ccdad0295a91898f1b866050a6c
Anwesh43 <anweshthecool0@gmail.com>
2017-08-02 20:34:50 +0530

    demo of selectable list in Activity

--- app/src/main/java/com/anwesome/ui/kotlinselecltablelistdemo/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/kotlinselecltablelistdemo/MainActivity.kt
@@ -2,11 +2,25 @@ package com.anwesome.ui.kotlinselecltablelistdemo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import com.anwesome.ui.selectablelistkotlin.SelectableList
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        SelectableList.create(this)
+        SelectableList.addSelectableItem("Hello")
+        SelectableList.addSelectableItem("Iron Man")
+        SelectableList.addSelectableItem("Spider Man")
+        SelectableList.addSelectableItem("Hello")
+        SelectableList.addSelectableItem("Iron Man")
+        SelectableList.addSelectableItem("Spider Man")
+        SelectableList.addSelectableItem("Hello")
+        SelectableList.addSelectableItem("Iron Man")
+        SelectableList.addSelectableItem("Spider Man")
+        SelectableList.addSelectableItem("Super Man")
+        //addContentView(list, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT))
     }
 }
