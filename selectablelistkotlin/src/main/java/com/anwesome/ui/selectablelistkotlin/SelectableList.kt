20f9278fc5fa0d544fa29c1adfe96937f7b92b49
Anwesh43 <anweshthecool0@gmail.com>
2017-08-02 19:21:20 +0530

    adding SelectableList and SelectableView

---
+++ selectablelistkotlin/src/main/java/com/anwesome/ui/selectablelistkotlin/SelectableList.kt
@@ -0,0 +1,40 @@
+package com.anwesome.ui.selectablelistkotlin
+
+import android.content.Context
+import android.graphics.Point
+import android.hardware.display.DisplayManager
+import android.view.ViewGroup
+
+/**
+ * Created by anweshmishra on 02/08/17.
+ */
+class SelectableList(context:Context,var deviceW:Int = 0,var deviceH:Int = 0):ViewGroup(context) {
+    init {
+        getDimension(context)
+    }
+    private fun getDimension(context: Context) {
+        var displayManager = context.getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+        var display = displayManager.getDisplay(0)
+        var size = Point()
+        display.getRealSize(size)
+        deviceW = size.x
+        deviceH = size.y
+    }
+    override fun onMeasure(wspec:Int,hspec:Int) {
+        var hView = deviceH/20
+        for(i in 0..childCount-1) {
+            var view = getChildAt(i)
+            measureChild(view,wspec,hspec)
+            hView += view.measuredHeight + deviceH/20
+        }
+        setMeasuredDimension(deviceW,hView)
+    }
+    override fun onLayout(reloaded:Boolean,a:Int,b:Int,w:Int,h:Int) {
+        var y = deviceH/20
+        for(i in 0..childCount-1) {
+            var view = getChildAt(i)
+            view.layout(0,y,w,y+view.measuredHeight)
+            y+=view.measuredHeight
+        }
+    }
+}
---
+++ selectablelistkotlin/src/main/java/com/anwesome/ui/selectablelistkotlin/SelectableView.kt
@@ -0,0 +1,76 @@
+package com.anwesome.ui.selectablelistkotlin
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.RectF
+import android.view.MotionEvent
+import android.view.View
+
+/**
+ * Created by anweshmishra on 02/08/17.
+ */
+class SelectableView(ctx:Context,var text:String):View(ctx) {
+    var time = 0
+    val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    var selectableItem:SelectableItem?=null
+    override fun onDraw(canvas:Canvas) {
+        if(time == 0) {
+            selectableItem = SelectableItem(text,canvas.width.toFloat(),canvas.height.toFloat())
+        }
+        selectableItem?.draw(canvas,paint)
+        time++
+    }
+    fun update() {
+        selectableItem?.update()
+        postInvalidate()
+    }
+    override fun onTouchEvent(event:MotionEvent):Boolean {
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                selectableItem?.startUpdating()
+            }
+        }
+        return true
+    }
+    data class SelectableItem(var text:String,var w:Float,var h:Float,var scale:Float = 0.0f,var dir:Int = 0) {
+        fun update() {
+            scale += 0.2f*dir
+            if(scale > 1) {
+                scale = 1.0f
+                dir = 0
+            }
+            if(scale < 0) {
+                scale = 0.0f
+                dir = 0
+            }
+        }
+        fun stoppedUpdating():Boolean = dir == 0
+        fun draw(canvas:Canvas,paint:Paint) {
+            paint.style = Paint.Style.STROKE
+            paint.color = Color.parseColor("#4caf50")
+            paint.strokeWidth = w/60
+            paint.strokeCap = Paint.Cap.ROUND
+            canvas.drawRect(0.0f,0.0f,w,h,paint)
+            paint.style = Paint.Style.FILL
+            canvas.save()
+            canvas.translate(w/2,h/2)
+            canvas.save()
+            canvas.scale(scale,scale)
+            canvas.drawRect(RectF(-w/2,-h/2,w/2,h/2),paint)
+            canvas.restore()
+            paint.color = Color.WHITE
+            paint.textSize = w/20
+            canvas.drawText(text,-paint.measureText(text)/2,paint.textSize/2,paint)
+            canvas.restore()
+        }
+        fun startUpdating() {
+            dir = when(scale) {
+                0.0f -> 1
+                1.0f -> -1
+                else -> dir
+            }
+        }
+    }
+}
