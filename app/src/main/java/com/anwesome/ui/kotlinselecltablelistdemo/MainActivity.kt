de4d5c82e05f5552456e4fa00dd74dd69ee5f23e
Anwesh43 <anweshthecool0@gmail.com>
2017-08-03 00:06:25 +0530

    using SelectableList in MainActivity

--- app/src/main/java/com/anwesome/ui/kotlinselecltablelistdemo/MainActivity.kt
+++ app/src/main/java/com/anwesome/ui/kotlinselecltablelistdemo/MainActivity.kt
@@ -3,6 +3,8 @@ package com.anwesome.ui.kotlinselecltablelistdemo
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
+import android.widget.Toast
+import com.anwesome.ui.selectablelistkotlin.OnSelectedListener
 import com.anwesome.ui.selectablelistkotlin.SelectableList
 
 class MainActivity : AppCompatActivity() {
@@ -11,16 +13,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         SelectableList.create(this)
-        SelectableList.addSelectableItem("Hello")
-        SelectableList.addSelectableItem("Iron Man")
-        SelectableList.addSelectableItem("Spider Man")
-        SelectableList.addSelectableItem("Hello")
-        SelectableList.addSelectableItem("Iron Man")
-        SelectableList.addSelectableItem("Spider Man")
-        SelectableList.addSelectableItem("Hello")
-        SelectableList.addSelectableItem("Iron Man")
-        SelectableList.addSelectableItem("Spider Man")
-        SelectableList.addSelectableItem("Super Man")
+        var items:Array<String> = arrayOf("Wonder Woman","Iron Man","Spider Man","Super Man","Ant Man","Bat Man","Aqua Man","Green Arrow","Flash")
+        items.forEach { item ->
+            SelectableList.addSelectableItem(item,CustomSelectedListener(this,item))
+        }
         //addContentView(list, ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT))
+    }
+    class CustomSelectedListener(var activity: MainActivity,var text:String):OnSelectedListener {
+        override fun onSelect(text: String) {
+            Toast.makeText(activity,"selected $text",Toast.LENGTH_SHORT).show()
+        }
+
+        override fun onUnSelect(text: String) {
+            Toast.makeText(activity,"unselected $text",Toast.LENGTH_SHORT).show()
+        }
     }
 }
